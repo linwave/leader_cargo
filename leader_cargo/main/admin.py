ec6fd1707b2101b844c509e673224607aa216194
linwave <avks.oleg@gmail.com>
2023-10-17 18:10:47 +0300

    Изменение админ панели

--- leader_cargo/main/admin.py
+++ leader_cargo/main/admin.py
@@ -3,7 +3,7 @@ from .models import CustomUser, Appeals, Goods
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'phone', 'role', 'time_create', 'status')
+    list_display = ('id', 'last_name', 'first_name', 'patronymic', 'phone', 'role', 'time_create', 'status')
     list_display_links = ('id', 'phone')
     search_fields = ('role', 'phone')
     list_editable = ('status',)
