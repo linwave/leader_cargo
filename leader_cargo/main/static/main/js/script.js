64415e975e5549b0cde161bb7468b1e70aa44af1
linwave <avks.oleg@gmail.com>
2023-11-22 17:24:29 +0300

    Доп к курсам

--- leader_cargo/main/static/main/js/script.js
+++ leader_cargo/main/static/main/js/script.js
@@ -37,6 +37,27 @@ function yuan_pattern(){
 };
 var mask_yuan = IMask(yuan, yuan_options);
 }
+
+function yuan_non_cash_pattern(){
+    var yuan = document.getElementById('yuan_non_cash');
+    var yuan_options = {
+    mask: Number,  // enable number mask
+
+    // other options are optional with defaults below
+    scale: 2,  // digits after point, 0 for integers
+    thousandsSeparator: '',  // any single char
+    padFractionalZeros: false,  // if true, then pads zeros at end to the length of scale
+    normalizeZeros: true,  // appends or removes zeros at ends
+    radix: '.',  // fractional delimiter
+    mapToRadix: ['.'],  // symbols to process as radix
+
+    // additional number interval options (e.g.)
+    min: -100,
+    max: 100
+};
+var mask_yuan = IMask(yuan, yuan_options);
+}
+
 function dollar_pattern(){
 var dollar = document.getElementById('dollar');
 var dollar_options = {
